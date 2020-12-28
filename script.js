9df3e7b8fad371fc160d2b5ad01dbdbfcb52c6d6
Jose <jose42cordova@gmail.com>
2020-12-28 17:14:31 -0600

    added char arrays, random vars, and prompts

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 // Assignment Code
+var lcLetters = "abcdefghijklmnopqrstuvwxyz";
+// var ucLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numerics = "0123456789";
+var specials = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+// var allChars = [lcLetters, ucLetters, numerics, specials];
+
+// alert(allChars[Math.floor(Math.random()*allChars.length)][Math.floor(Math.random()*33)]);
+
+var randLow = lcLetters[Math.floor(Math.random()*lcLetters.length)];
+var randUp = lcLetters[Math.floor(Math.random()*lcLetters.length)].toUpperCase();
+var randNum = numerics[Math.floor(Math.random()*numerics.length)];
+var randSpec = specials[Math.floor(Math.random()*specials.length)];
+
+// alert(randLow);
+// alert(randUp);
+// alert(randNum);
+// alert(randSpec);
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +30,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+alert("Get ready for your random password!");
+confirm("Do you want lowercase letters?");
+confirm("Do you want uppercase letters?");
+confirm("Do you want numbers?");
+confirm("Do you want special characters?");
