ad7b3f622f588fc1be9e41bef50fa12b4b40f5c1
Jose <jose42cordova@gmail.com>
2020-12-31 14:26:28 -0600

    added comments to js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 var lcLetters = "abcdefghijklmnopqrstuvwxyz";
 var numerics = "0123456789";
 var specials = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var userChoices = [];
+var userChoices = []; // filled with strings by getChars() (line 19) and used in for-loop (lines 64-72)
 
-function getPwLength () {
+  //8-16 function returns password length after making sure it is between 8 and 128 characters
+function getPwLength () { // called on line 59
   var pwLength = prompt("How long do you want your password to be? You can choose between 8 and 128 characters.");
   if (pwLength < 8 || pwLength > 128) {
     alert("Must be between 8 and 128 characters!");
@@ -14,15 +15,16 @@ function getPwLength () {
   }
 };
 
-function getChars () {
+  //19-41 function for determining password criteria
+function getChars () { // called on line 60
   var low = confirm("Do you want lowercase letters?");
   var upp = confirm("Do you want uppercase letters?");
   var num = confirm("Do you want numbers?");
   var spec = confirm("Do you want special characters?");
-  if (low === false && upp === false && num === false && spec === false) {
+  if (low === false && upp === false && num === false && spec === false) { // makes sure user picks at least one type
     alert("Must choose at least one type of character!");
     getChars();
-  } else {
+  } else { // pushes respective string to empty userChoices array (line 5)
     if (low) {
       userChoices.push("low")
     }
@@ -38,29 +40,28 @@ function getChars () {
   }
 };
 
-function randLow () {
+  //44-55 functions for returning random lowercase, uppercase, number, and special characters
+function randLow () { // potentially called on line 65
   return lcLetters[Math.floor(Math.random()*lcLetters.length)].toLowerCase();
 };
-
-function randUp () {
+function randUp () { // potentially called on line 67
   return lcLetters[Math.floor(Math.random()*lcLetters.length)].toUpperCase();
 };
-
-function randNum () {
+function randNum () { // potentially called on line 69
   return numerics[Math.floor(Math.random()*numerics.length)];
 };
-
-function randSpec () {
+function randSpec () { // potentially called on line 71
   return specials[Math.floor(Math.random()*specials.length)];
 };
 
+  //58-75 function for the actual generation of the password based on the length and criteria the user chooses
 function generatePassword() {
-  var pwLength = getPwLength();
-  getChars();
-  var pwString = "";
-  for (var i = 0; i < pwLength; i++) {
-    var charOpts = userChoices[Math.floor(Math.random() * userChoices.length)];
-    if (charOpts === "low") {
+  var pwLength = getPwLength(); // line 8 function is called and return is used for local variable pwLength
+  getChars(); // line 19 function is called
+  var pwString = ""; // empty string to be filled by for-loop (lines 62-73)
+  for (var i = 0; i < pwLength; i++) { // pwLength determines how many times to run loop
+    var charOpts = userChoices[Math.floor(Math.random() * userChoices.length)]; // chooses random index from userChoices (line 5) that now has at least one value
+    if (charOpts === "low") { // strings that ended up in empty array userChoices (line 5) now determine what functions from lines 44-45 will be called to build the password
       pwString = pwString + randLow();
     } else if (charOpts === "upp") {
       pwString = pwString + randUp();
@@ -70,7 +71,7 @@ function generatePassword() {
       pwString = pwString + randSpec();
     } 
   }
-  return pwString;
+  return pwString; // used in line 81
 };
 
 var generateBtn = document.querySelector("#generate");
